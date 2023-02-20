d45b447b8e5bb34eb81568e46fabe9e00bc83192
Kyle Deal <kdeal@kyledeal.com>
2023-02-19 23:09:42 -0500

    Create devenv docker image
    Right now going to make it only really useful for doing some kubernetes
    work.

---
+++ devenv/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup ssh user
+adduser --disabled-password --gecos "" --shell /bin/fish "$SSH_USER"
+mkdir -p "/home/$SSH_USER/.ssh/"
+echo "$SSH_PUB_KEY" > "/home/$SSH_USER/.ssh/authorized_keys"
+
+# Setup ssh server
+# Copy host keys into config dir
+test -d /config && cp /config/*_key* /etc/ssh/
+mkdir --mode=0755 /run/sshd
+
+echo "Starting ssh server"
+exec /usr/sbin/sshd -D
