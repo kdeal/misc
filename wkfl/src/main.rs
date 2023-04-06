472e253158b970f9ff882f9143fc1ec44b493ba5
Kyle Deal <kdeal@kyledeal.com>
2023-04-06 05:25:08 +0000

    Make actions take at most a context or config
    This moves some code out of the main file and makes the match statement
    a little smaller. It was a little inconsistent on what showed prompts in
    the main.rs file and what did it in the action, so I moved them all to
    the action.

--- wkfl/src/main.rs
+++ wkfl/src/main.rs
@@ -68,25 +68,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         shell_actions: vec![],
     };
     match cli.command {
-        Commands::Start => {
-            let repo = git::get_repository()?;
-            let name = prompts::basic_prompt("Name:")?;
-            let ticket_str = prompts::basic_prompt("Ticket:")?;
-            let ticket = if ticket_str.is_empty() {
-                None
-            } else {
-                Some(ticket_str)
-            };
-            actions::start_workflow(repo, &name, &ticket, &mut context)?
-        }
-        Commands::End => {
-            let repo = git::get_repository()?;
-            actions::end_workflow(repo)?;
-        }
-        Commands::RepoDebug => {
-            let repo = git::get_repository()?;
-            actions::print_repo_debug_info(repo)?;
-        }
+        Commands::Start => actions::start_workflow(&mut context)?,
+        Commands::End => actions::end_workflow()?,
+        Commands::RepoDebug => actions::print_repo_debug_info()?,
         Commands::Repos => actions::list_repositories(context.config)?,
         Commands::Repo => actions::switch_repo(&mut context)?,
         Commands::Clone => actions::clone_repo(&mut context)?,
