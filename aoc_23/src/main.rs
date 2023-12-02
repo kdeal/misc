2b8f9e674d1308b3d86d76810370f74bb51a94f7
Kyle Deal <kdeal@kyledeal.com>
2023-12-02 05:09:42 +0000

    Make project support multiple projects

--- aoc_23/src/main.rs
+++ aoc_23/src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::fs;
 
 fn extract_calibration_numbers(line: &str) -> u32 {
-    eprintln!("line: {}", line);
     if let Some(first_digit_str) = line.chars().find(|c| c.is_ascii_digit()) {
         let first_digit = first_digit_str.to_digit(10).unwrap();
         let last_digit = line
@@ -18,9 +17,7 @@ fn extract_calibration_numbers(line: &str) -> u32 {
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let problem = &args[1];
+fn day1_problem_a(args: Vec<String>) {
     let file_path = &args[2];
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let result: u32 = contents
@@ -28,4 +25,12 @@ fn main() {
         .map(|line| extract_calibration_numbers(line))
         .sum();
     println!("{}", result);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let problem = &args[1];
+    match problem {
+        "1a" => day1_problem_a(args),
+    };
 }
