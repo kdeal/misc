c66b9a53e55dc388cd56eb0a61c10b7228712cbe
Kyle Deal <kdeal@kyledeal.com>
2023-12-13 04:47:43 +0000

    Add solution for day 4 problem b

--- aoc_23/src/day4.rs
+++ aoc_23/src/day4.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 struct Card {
     id: u32,
     winning_numbers: Vec<u32>,
     card_numbers: Vec<u32>,
+    num_matching: u32,
 }
 
 fn parse_numbers(numbers_str: &str) -> Vec<u32> {
@@ -21,24 +24,26 @@ fn parse_card(line: &str) -> Card {
         .parse::<u32>()
         .unwrap();
     let (card_number_str, winning_numbers_str) = numbers_str.split_once("|").unwrap();
+    let winning_numbers = parse_numbers(winning_numbers_str);
+    let card_numbers = parse_numbers(card_number_str);
+    let usize_matching = card_numbers
+        .iter()
+        .filter(|number| winning_numbers.contains(&number))
+        .count();
     Card {
         id: card_id,
-        winning_numbers: parse_numbers(winning_numbers_str),
-        card_numbers: parse_numbers(card_number_str),
+        winning_numbers,
+        card_numbers,
+        num_matching: u32::try_from(usize_matching).unwrap(),
     }
 }
 
 fn calculate_card_score(card: Card) -> u32 {
-    let num_matching = card
-        .card_numbers
-        .iter()
-        .filter(|number| card.winning_numbers.contains(&number))
-        .count();
-    if num_matching == 0 {
+    if card.num_matching == 0 {
         return 0;
     }
     let base: i32 = 2;
-    let result = base.pow(u32::try_from(num_matching - 1).unwrap());
+    let result = base.pow(card.num_matching - 1);
     result.try_into().unwrap()
 }
 
@@ -49,4 +54,29 @@ pub fn problem_a(contents: String) -> u32 {
         .map(|line| parse_card(line))
         .map(|card| calculate_card_score(card))
         .sum()
+}
+
+pub fn problem_b(contents: String) -> u32 {
+    let cards = contents
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| parse_card(line));
+
+    let card_num_to_card: HashMap<u32, Card> =
+        cards.into_iter().map(|card| (card.id, card)).collect();
+
+    let mut num_cards = 0;
+    let mut cards_to_process: Vec<u32> = card_num_to_card
+        .keys()
+        .map(|card_id| card_id.clone())
+        .collect();
+    while let Some(card_id) = cards_to_process.pop() {
+        num_cards += 1;
+        let card = card_num_to_card.get(&card_id).unwrap();
+        let num_card_matches = card.num_matching;
+        for copy_offset in 0..num_card_matches {
+            cards_to_process.push(card_id + 1 + copy_offset);
+        }
+    }
+    num_cards
 }
