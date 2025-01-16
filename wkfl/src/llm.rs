20d54c07719c170ec718ae0262172f6d76a4b2e4
Kyle Deal <kdeal@kyledeal.com>
2025-01-16 04:58:05 +0000

    Add some enums and rename some structs

--- wkfl/src/llm.rs
+++ wkfl/src/llm.rs
@@ -1,1 +1,18 @@
+use serde::{Deserialize, Serialize};
+
 pub mod perplexity;
+
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Role {
+    Assistant,
+    System,
+    User,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Message {
+    pub role: Role,
+    pub content: String,
+}
