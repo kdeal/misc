72446a1d3ad4bd59f074b188a9d6df354f836168
Kyle Deal <kdeal@kyledeal.com>
2025-01-22 05:30:31 +0000

    Create function for handle path staring with ~/

--- wkfl/src/config.rs
+++ wkfl/src/config.rs
@@ -38,31 +38,11 @@ pub struct RepoConfig {
 
 impl Config {
     pub fn repositories_directory_path(&self) -> anyhow::Result<PathBuf> {
-        if self.repositories_directory.starts_with("~/") {
-            let mut repo_path = home_dir().ok_or(anyhow::anyhow!("Can't determine home dir"))?;
-            let no_prefix_repos_dir = self
-                .repositories_directory
-                .strip_prefix("~/")
-                .expect("Checked that it had the prefix above. Should be safe");
-            repo_path.push(no_prefix_repos_dir);
-            Ok(repo_path)
-        } else {
-            Ok(PathBuf::from(&self.repositories_directory))
-        }
+        create_path_from_string(&self.repositories_directory)
     }
     pub fn notes_directory_path(&self) -> anyhow::Result<PathBuf> {
         if let Some(notes_directory) = &self.notes_directory {
-            if notes_directory.starts_with("~/") {
-                let mut repo_path =
-                    home_dir().ok_or(anyhow::anyhow!("Can't determine home dir"))?;
-                let no_prefix_repos_dir = notes_directory
-                    .strip_prefix("~/")
-                    .expect("Checked that it had the prefix above. Should be safe");
-                repo_path.push(no_prefix_repos_dir);
-                Ok(repo_path)
-            } else {
-                Ok(PathBuf::from(&self.repositories_directory))
-            }
+            create_path_from_string(&notes_directory)
         } else {
             let mut notes_directory_path = self.repositories_directory_path()?;
             notes_directory_path.push("notes");
@@ -73,6 +53,21 @@ impl Config {
 
 fn default_repo_base_dir() -> String {
     "~/repos/".to_string()
+}
+
+/// Creates a PathBuf from a string. Handles converting ~/ to home dir
+fn create_path_from_string(path_str: &str) -> anyhow::Result<PathBuf> {
+    if path_str.starts_with("~/") {
+        let mut path = home_dir().ok_or(anyhow::anyhow!("Can't determine home dir"))?;
+        let no_prefix_path = path_str
+            .strip_prefix("~/")
+            .expect("Checked that it had the prefix above. Should be safe");
+        path.push(no_prefix_path);
+        Ok(path)
+    } else {
+        Ok(PathBuf::from(path_str))
+    }
+
 }
 
 pub fn get_config() -> anyhow::Result<Config> {
