9bc85fd27002a37514ad2de5fe208a3093b0d7b1
Kyle Deal <kdeal@kyledeal.com>
2025-06-19 04:59:05 +0000

    Add fmt command
    This is the same as the test command, but the intention is that these
    commands will actually change files.

--- wkfl/src/main.rs
+++ wkfl/src/main.rs
@@ -42,6 +42,8 @@ enum Commands {
     PruneBranches,
     /// Run test commands defined in repo config
     Test,
+    /// Run fmt commands defined in repo config
+    Fmt,
     Confirm {
         #[arg(value_hint = ValueHint::Other)]
         prompt: Option<String>,
@@ -156,6 +158,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Commands::Clone => actions::clone_repo(&mut context)?,
         Commands::PruneBranches => actions::prune_merged_branches(&context.config)?,
         Commands::Test => actions::run_test_commands(&mut context)?,
+        Commands::Fmt => actions::run_fmt_commands(&mut context)?,
         Commands::Config => actions::print_config(context.config),
         Commands::Confirm {
             prompt: user_prompt,
