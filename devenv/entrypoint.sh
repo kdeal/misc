ab91aaade3a8763415d585cc3d3fa38df7b23250
Kyle Deal <kdeal@kyledeal.com>
2026-03-06 11:45:47 -0500

    Update devenv to also install go, rust, and podman (#244)
    This will allow me to use this in more places.

--- devenv/entrypoint.sh
+++ devenv/entrypoint.sh
@@ -3,9 +3,14 @@
 # Setup ssh user
 addgroup --gid "$SSH_GROUP_ID" "$SSH_USER"
 adduser --disabled-password --gecos "" --uid "$SSH_USER_ID" --gid "$SSH_GROUP_ID" "$SSH_USER"
+
 mkdir -p "/home/$SSH_USER/.ssh/"
 chown "$SSH_USER:$SSH_USER" "/home/$SSH_USER/.ssh/"
 echo "$SSH_PUB_KEY" > "/home/$SSH_USER/.ssh/authorized_keys"
+
+mv $RUSTUP_HOME "/home/$SSH_USER/.rustup"
+chown -R "$SSH_USER:$SSH_USER" "/home/$SSH_USER/.rustup"
+chmod -R og-wx "/home/$SSH_USER/.rustup"
 
 # Setup ssh server
 # Copy host keys into config dir
